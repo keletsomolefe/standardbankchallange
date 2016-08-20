e6ae3c3dcc48d1430af915038826d66c033d2003
keletso <patrickkeletso@gmail.com>
2016-08-20 22:07:59 +0200

    LIST and REMOVE

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/', function (req, res) {
-	var xyz = {"response": {
-   "outputSpeech": {
-     "type": "PlainText",
-     "text": "Choki, I am very sorry, I know I hurt you and I know I'm not the best of people, but I care about you and I hope you can forgive me"
-   },
-   "shouldEndSession": true
- }
-}
-
-
-  res.json(xyz);
+	console.log(req.body.request.type=="IntentRequest");
+	if (req.body.request.intent.name=="Remove"){
+		var xyz = {	
+					"response": {
+					"outputSpeech": {
+						"type": "PlainText",
+						"text": "Item removed from the list"
+					},
+					"shouldEndSession": true
+				}
+			};
+			res.json(xyz);
+	}
+	else if (req.body.request.intent.name=="List"){
+		var xyz = {	
+					"response": {
+					"outputSpeech": {
+						"type": "PlainText",
+						"text": "Here is your list"
+					},
+					"shouldEndSession": true
+				}
+			};
+			res.json(xyz);
+	}
 });
 
 app.listen(3000, function () {
