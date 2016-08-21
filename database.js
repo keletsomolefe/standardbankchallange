ed3c4dd58ece7a1e68731784ea75aa0358a25649
tbddorothy <dimphomahoko@gmail.com>
2016-08-21 14:46:44 +0200

    compiles

--- database.js
+++ database.js
@@ -9,20 +9,18 @@ var conn = mysql.createConnection({
 });
 	
 	
-	conn.connect();
-
-	var request = new sql.Request();
-	var sqlstmt;
-
-	
-	function initializeDB()
+	conn.connect(function(err)
 	{
+		if(err)
+		{	
+			console.log('Error connecting to the database')
+			return;
+		}
+		console.log('Connection established');	
+	});
+
+	var sqlstmt;
 		
-		sqlstmt = "SELECT `fullname` FROM `Users` LIMIT 1";
-		
-		
-			
-			
 
 
 	function addItem(Item)
@@ -31,7 +29,7 @@ var conn = mysql.createConnection({
 		conn.query(sqlstmt, function(err,rows)
 		{
 			numRows = rows.length;
-		})
+		});
 	
 		if(numRows = 0)
 		{	
@@ -47,6 +45,57 @@ var conn = mysql.createConnection({
 		else
 		{
 			return Item + " already exists in your Viands List";
-		}	
+		}
+	}
+
+	function removeItem(Item)
+	{
+		sqlstmt = "SELECT `item_ID` FROM `List` WHERE 'item_Name' = 'Item'";
+		conn.query(sqlstmt, function(err,rows)
+		{
+			numRows = rows.length;
+		});
+	
+		if(numRows > 0)
+		{	
+			sqlstmt = "DELETE FROM 'List' WHERE 'item_Name' = 'Item'";
+
+			conn.query(sqlstmt, function(err,rows)
+			{
+				if(err) throw err;		
+			});
+
+			return Item + " has been removed to your Viands List";
+		}
+		else
+		{
+			return Item + " is not in your Viands List";
+		}
+	}
+
+	function listItems()
+	{
+		sqlstmt = "SELECT 'item_Name' FROM 'List'";
+
+		conn.query(sqlstmt, function(err,rows)
+		{
+			numRows = rows.length;
+		});
+	
+		if(numRows > 0)
+		{	
+			return "Here is your Viands list.";
+		}
+		else
+		{
+			return "Your Viands List is empty";
+		}
+
 
 	}
+
+	function processOrder()
+	{
+		
+	}
+
